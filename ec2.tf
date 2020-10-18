b07eeac9b45114cd54cf4e15fc6174166aef385d
pyenugula <pkyenugula@gmail.com>
2020-10-18 15:45:37 +0530

    Depoly a code to webserver using Terraform

--- ec2.tf
+++ ec2.tf
@@ -11,6 +11,16 @@ resource "aws_instance" "my-ec2" {
   ami = "ami-0732b62d310b80e97"
   instance_type = "t2.micro"
 
+  # boot straping
+  user_data = <<-EOF
+              #! /bin/bash
+              sudo yum install -y httpd git
+              sudo service httpd start
+              sudo chkconfig httpd on
+              cd /var/www/html
+              sudo git clone https://github.com/sreesysadm/carwebsite.git .
+              sudo service httpd restart
+              EOF
 
   tags = {
 
