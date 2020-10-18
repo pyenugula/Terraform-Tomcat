d5e9c88a140f2c2d4cc69ccc74d5f16772e6fad4
pyenugula <pkyenugula@gmail.com>
2020-10-18 16:10:41 +0530

    created security group for web server

--- ec2.tf
+++ ec2.tf
@@ -10,8 +10,10 @@ provider "aws" {
 resource "aws_instance" "my-ec2" {
   ami = "ami-0732b62d310b80e97"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my-ec2-sg.id]
 
   # boot straping
+
   user_data = <<-EOF
               #! /bin/bash
               sudo yum install -y httpd git
@@ -21,9 +23,32 @@ resource "aws_instance" "my-ec2" {
               sudo git clone https://github.com/sreesysadm/carwebsite.git .
               sudo service httpd restart
               EOF
-
   tags = {
 
     Name = "terraform-tomcat"
   }
+}
+
+#Security group
+
+resource "aws_security_group" "my-ec2-sg" {
+  name = "terraform-webserver-sg"
+
+  #Receive Trafic on port 80
+
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #outgoing trafic
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
